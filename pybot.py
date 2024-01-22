54bb22d6e4560ad6ee8951019b24c93d10743718
DanilProggg <123116138+DanilProggg@users.noreply.github.com>
2024-01-22 12:46:54 +0300

    Add files via upload

---
+++ pybot.py
@@ -0,0 +1,23 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="6832224846:AAEcZX5Qygr3k7ariXG4n2X5_9pWs-l3FzE")
+# Диспетчер
+dp = Dispatcher() #обработчик
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
